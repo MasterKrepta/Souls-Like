084858911d0c3e7251564d7ac054a53649e86a25
MasterKrepta <brianj.pajak@gmail.com>
2018-05-25 21:23:39 -0400

    End of Part 2
    Seems to be a bug when running, may have missed something at this point

---
+++ Assets/_Scripts/CameraManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    public static CameraManager singleton;
+
+    public bool lockOn;
+    public float followSpeed = 9;
+    public float mouseSpeed = 2;
+    public float controllerSpeed = 7;
+
+    public Transform target;
+
+    public Transform pivot;
+    public Transform camTrans;
+
+    float turnSmoothing = .1f;
+    public float minAngle = -35;
+    public float maxAngle = 35;
+    float smoothX;
+    float smoothY;
+    float smoothXVelocity;
+    float smoothYVelocity;
+    public float lookAngle;
+    public float tiltAngle;
+
+    public void Init(Transform t) {
+        target = t;
+        camTrans = Camera.main.transform;
+        pivot = camTrans.parent;
+    }
+
+    public void Tick(float d) {
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        float c_h = Input.GetAxis("RightAxisX");
+        float c_v = Input.GetAxis("RightAxisY");
+
+        float targetSpeed = mouseSpeed;
+
+        if(c_h != 0 || c_v !=0) {
+            h = c_h;
+            v = c_v;
+            targetSpeed = controllerSpeed;
+        }
+
+        FollowTarget(d);
+        HandleRotations(d, v, h, targetSpeed);
+    }
+    private void Awake() {
+        if(singleton != null) {
+            Destroy(gameObject);
+        }
+        singleton = this;
+    }
+
+    void FollowTarget(float d) {
+        float speed = d * followSpeed;
+        Vector3 targetPos = Vector3.Lerp(transform.position, target.position, speed);
+        transform.position = targetPos;
+    }
+
+    void HandleRotations(float d, float v, float h, float targetSpeed) {
+        if(turnSmoothing > 0) {
+            smoothX = Mathf.SmoothDamp(smoothX, h, ref smoothXVelocity, turnSmoothing);
+            smoothY = Mathf.SmoothDamp(smoothY, v, ref smoothYVelocity, turnSmoothing);
+        }
+        else {
+            smoothX = h;
+            smoothY = v;
+        }
+
+        if (lockOn) {
+
+        }
+
+        lookAngle += smoothX * targetSpeed;
+        transform.rotation = Quaternion.Euler(0, lookAngle, 0);
+
+        tiltAngle -= smoothY * targetSpeed;
+        tiltAngle = Mathf.Clamp(tiltAngle, minAngle, maxAngle);
+        pivot.localRotation = Quaternion.Euler(tiltAngle, 0, 0);
+    }
+}
---
+++ Assets/_Scripts/StateManager.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+    [Header("Init")]
+    public GameObject activeModel;
+
+    [Header("Inputs")]
+    public float vertical;
+    public float horizontal;
+    public float moveAmount;
+    public Vector3 moveDir;
+
+    [Header("Stats")]
+    public float moveSpeed = 3;
+    public float runSpeed = 3.5f;
+    public float rotSpeed = 5;
+    public float toGround = .5f;
+
+    [Header("States")]
+    public bool run;
+    public bool onGround;
+    public bool lockOn;
+
+    [HideInInspector]
+    public Animator anim;
+    [HideInInspector]
+    public Rigidbody rb;
+    [HideInInspector]
+    public float delta;
+    [HideInInspector]
+    public LayerMask ignoreLayers;
+
+
+    public void Init() {
+        SetupAnimator();
+        rb = GetComponent<Rigidbody>();
+        rb.angularDrag = 999;
+        rb.drag = 4;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        gameObject.layer = 8;
+        ignoreLayers = ~(1 << 9);
+
+        
+    }
+
+    void SetupAnimator() {
+        if (activeModel == null) {
+            anim.GetComponentInChildren<Animator>();
+            if (anim == null) {
+                Debug.LogWarning("No model found");
+            }
+        }
+        anim = activeModel.GetComponent<Animator>();
+
+    }
+
+    public void FixedTick(float d) {
+        delta = d;
+
+        rb.drag = (moveAmount > 0 || onGround == false) ? 0 : 4;
+
+        float targetSpeed = moveSpeed;
+        if (run) {
+            targetSpeed = runSpeed;
+        }
+
+        if (onGround) {
+            rb.velocity = moveDir * (targetSpeed * moveAmount);
+        }
+        if (run) {
+            lockOn = false;
+        }
+            
+
+        if (!lockOn) {
+            Vector3 targetDir = moveDir;
+            targetDir.y = 0;
+            if (targetDir == Vector3.zero) {
+                targetDir = transform.forward;
+            }
+
+            Quaternion tr = Quaternion.LookRotation(targetDir);
+            Quaternion targetRot = Quaternion.Slerp(transform.rotation, tr, delta * moveAmount * rotSpeed);
+            transform.rotation = targetRot;
+        }
+        
+
+        HandleMovementAnimations();
+    }
+
+
+    public void Tick(float d) {
+        delta = d;
+        onGround = OnGround();
+
+        anim.SetBool("onGround", onGround);
+    }
+    void HandleMovementAnimations() {
+        anim.SetBool("run", run);
+        anim.SetFloat("vertical", moveAmount, 0.4f, delta);
+
+    }
+
+    public bool OnGround() {
+        bool r = false;
+        Vector3 origin = transform.position + (Vector3.up * toGround);
+        Vector3 dir = -Vector3.up;
+        float dist = toGround + 0.3f;
+
+        RaycastHit hit;
+        if(Physics.Raycast(origin, dir, out hit, dist, ignoreLayers)) {
+            r = true;
+            Vector3 targetPos = hit.point;
+            transform.position = targetPos;
+        }
+
+        return r;
+
+    }
+}
